e2de25bc19d36a5ff0d8c665ec3394d60fb0004f
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-04-23 12:56:54 +0100

    Added SERVER_PORT to all SERVER_NAME instances
    This helps for local installs which are likely to be on ports other
    than 80, and it fixes the problem with the installer (as basic_finished
    had some links with port, some without).

--- installer/basic_finished.php
+++ installer/basic_finished.php
@@ -27,15 +27,15 @@
 		<div class="feedback success">
 			<h2>Installation Successful!</h2>
 		</div>
-		<p>To login, go to <a href="http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]); ?>/admin" target="_blank">http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]); ?>/admin</a> and use the following credentials:<br /><br />
+		<p>To login, go to <a href="http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"])."/admin";?>" target="_blank">http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index."/admin";?></a> and use the following credentials:<br /><br />
 			<strong>Username:</strong> admin<br />
 			<strong>Password:</strong> admin</p>
 			<p><strong>Other next steps...</strong></p>
 			<ul>
 				<li><a href="http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]); ?>" target="_blank">View your website</a></li>
-				<li><a href="http://<?php echo $_SERVER['SERVER_NAME']."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index;?>/admin/reports/edit" target="_blank">Upload report data</a></li>
-				<li><a href="http://<?php echo $_SERVER['SERVER_NAME']."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index;?>/admin/settings" target="_blank">Configure your map</a></li>	 
-				<li><a href="http://<?php echo $_SERVER['SERVER_NAME']."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index;?>/admin/settings/sms" target="_blank">Setup your SMS server</a></li>							
+				<li><a href="http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index;?>/admin/reports/edit" target="_blank">Upload report data</a></li>
+				<li><a href="http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index;?>/admin/settings" target="_blank">Configure your map</a></li>	 
+				<li><a href="http://<?php echo $_SERVER['SERVER_NAME'].":".$_SERVER["SERVER_PORT"]."/".$install->_get_base_path($_SERVER["REQUEST_URI"]).$index;?>/admin/settings/sms" target="_blank">Setup your SMS server</a></li>							
 			</ul>
 			   
   </div>
