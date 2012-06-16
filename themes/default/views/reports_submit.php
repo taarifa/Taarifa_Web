bdd7626a6a906aa25e73672b3ab3c311dd9dcd30
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-06-16 16:00:10 +0100

    Submit button at the end of the page

--- themes/default/views/reports_submit.php
+++ themes/default/views/reports_submit.php
@@ -33,117 +33,6 @@
 			<div class="row">
 				<input type="hidden" name="form_id" id="form_id" value="<?php echo $id?>">
 			</div>
-			<div class="report_left">
-				<div class="report_row wdif">
-					<?php if(count($forms) > 1){ ?>
-					<div class="row">
-						<h4><span><?php echo Kohana::lang('ui_main.select_form_type');?></span>
-						<span class="sel-holder">
-							<?php print form::dropdown('form_id', $forms, $form['form_id'],
-						' onchange="formSwitch(this.options[this.selectedIndex].value, \''.$id.'\')"') ?>
-						</span>
-						<div id="form_loader" style="float:left;"></div>
-						</h4>
-					</div>
-					<?php } ?>
-					<label for="incident_title" class="inlined" id="incident_title_label"><?php echo Kohana::lang('ui_main.reports_title'); ?></label>
-					<?php print form::input('incident_title', $form['incident_title'], ' class="text long"'); ?>
-				</div>
-				<div class="report_row wdif">
-					<label for="incident_description" class="inlined"><?php echo Kohana::lang('ui_main.reports_description'); ?></label>
-					<?php print form::textarea('incident_description', $form['incident_description'], ' rows="10" class="textarea long text" ') ?>
-				</div>
-				<div class="report_row wdif" id="datetime_default">
-					<h4>
-						<a href="#" id="date_toggle" class="wdif show-more"><?php echo Kohana::lang('ui_main.modify_date'); ?></a>
-						<?php echo Kohana::lang('ui_main.date_time'); ?>: 
-						<?php echo Kohana::lang('ui_main.today_at')." "."<span id='current_time'>".$form['incident_hour']
-							.":".$form['incident_minute']." ".$form['incident_ampm']."</span>"; ?>
-						<?php if($site_timezone != NULL): ?>
-							<small>(<?php echo $site_timezone; ?>)</small>
-						<?php endif; ?>
-					</h4>
-				</div>
-				<div class="report_row hide" id="datetime_edit">
-					<div class="date-box">
-						<h4><?php echo Kohana::lang('ui_main.reports_date'); ?></h4>
-						<?php print form::input('incident_date', $form['incident_date'], ' class="text short"'); ?>
-						<script type="text/javascript">
-							$().ready(function() {
-								$("#incident_date").datepicker({ 
-									showOn: "both", 
-									buttonImage: "<?php echo url::file_loc('img'); ?>media/img/icon-calendar.gif", 
-									buttonImageOnly: true 
-								});
-							});
-						</script>
-					</div>
-					<div class="time">
-						<h4><?php echo Kohana::lang('ui_main.reports_time'); ?></h4>
-						<?php
-							for ($i=1; $i <= 12 ; $i++)
-							{ 
-								$hour_array[sprintf("%02d", $i)] = sprintf("%02d", $i);	 // Add Leading Zero
-							}
-							for ($j=0; $j <= 59 ; $j++)
-							{ 
-								$minute_array[sprintf("%02d", $j)] = sprintf("%02d", $j);	// Add Leading Zero
-							}
-							$ampm_array = array('pm'=>'pm','am'=>'am');
-							print form::dropdown('incident_hour',$hour_array,$form['incident_hour']);
-							print '<span class="dots">:</span>';
-							print form::dropdown('incident_minute',$minute_array,$form['incident_minute']);
-							print '<span class="dots">:</span>';
-							print form::dropdown('incident_ampm',$ampm_array,$form['incident_ampm']);
-						?>
-						<?php if ($site_timezone != NULL): ?>
-							<small>(<?php echo $site_timezone; ?>)</small>
-						<?php endif; ?>
-					</div>
-					<div style="clear:both; display:block;" id="incident_date_time"></div>
-				</div>
-				<div class="report_row wdif">
-					<h4><?php echo Kohana::lang('ui_main.reports_categories'); ?></h4>
-					<div class="report_category" id="categories">
-					<?php
-						$selected_categories = (!empty($form['incident_category']) AND is_array($form['incident_category']))
-							? $selected_categories = $form['incident_category']
-							: array();
-							
-						$columns = 2;
-						echo category::tree($categories, $selected_categories, 'incident_category', $columns);
-						?>
-					</div>
-				</div>
-
-
-				<?php
-				// Action::report_form - Runs right after the report categories
-				Event::run('ushahidi_action.report_form');
-				?>
-
-				<?php echo $custom_forms ?>
-
-				<div class="report_optional wdif">
-					<h3><?php echo Kohana::lang('ui_main.reports_optional'); ?></h3>
-					<div class="report_row">
-            <label for="person_first" class="inlined"><?php echo Kohana::lang('ui_main.reports_first'); ?></label>
-						<?php print form::input('person_first', $form['person_first'], ' class="text long"'); ?>
-					</div>
-					<div class="report_row">
-						<label for="person_last" class="inlined"><?php echo Kohana::lang('ui_main.reports_last'); ?></label>
-						<?php print form::input('person_last', $form['person_last'], ' class="text long"'); ?>
-					</div>
-					<div class="report_row">
-						<label for="person_email" class="inlined"><?php echo Kohana::lang('ui_main.reports_email'); ?></label>
-						<?php print form::input('person_email', $form['person_email'], ' class="text long"'); ?>
-					</div>
-					<?php
-					// Action::report_form_optional - Runs in the optional information of the report form
-					Event::run('ushahidi_action.report_form_optional');
-					?>
-				</div>
-			</div>
 			<div class="report_right">
 				<?php if ( ! $multi_country AND count($cities) > 1): ?>
 				<div class="report_row">
@@ -307,11 +196,122 @@
 
 				</div>
 									
+						</div>
+		<div class="report_left">
+				<div class="report_row wdif">
+					<?php if(count($forms) > 1){ ?>
+					<div class="row">
+						<h4><span><?php echo Kohana::lang('ui_main.select_form_type');?></span>
+						<span class="sel-holder">
+							<?php print form::dropdown('form_id', $forms, $form['form_id'],
+						' onchange="formSwitch(this.options[this.selectedIndex].value, \''.$id.'\')"') ?>
+						</span>
+						<div id="form_loader" style="float:left;"></div>
+						</h4>
+					</div>
+					<?php } ?>
+					<label for="incident_title" class="inlined" id="incident_title_label"><?php echo Kohana::lang('ui_main.reports_title'); ?></label>
+					<?php print form::input('incident_title', $form['incident_title'], ' class="text long"'); ?>
+				</div>
+				<div class="report_row wdif">
+					<label for="incident_description" class="inlined"><?php echo Kohana::lang('ui_main.reports_description'); ?></label>
+					<?php print form::textarea('incident_description', $form['incident_description'], ' rows="10" class="textarea long text" ') ?>
+				</div>
+				<div class="report_row wdif" id="datetime_default">
+					<h4>
+						<a href="#" id="date_toggle" class="wdif show-more"><?php echo Kohana::lang('ui_main.modify_date'); ?></a>
+						<?php echo Kohana::lang('ui_main.date_time'); ?>: 
+						<?php echo Kohana::lang('ui_main.today_at')." "."<span id='current_time'>".$form['incident_hour']
+							.":".$form['incident_minute']." ".$form['incident_ampm']."</span>"; ?>
+						<?php if($site_timezone != NULL): ?>
+							<small>(<?php echo $site_timezone; ?>)</small>
+						<?php endif; ?>
+					</h4>
+				</div>
+				<div class="report_row hide" id="datetime_edit">
+					<div class="date-box">
+						<h4><?php echo Kohana::lang('ui_main.reports_date'); ?></h4>
+						<?php print form::input('incident_date', $form['incident_date'], ' class="text short"'); ?>
+						<script type="text/javascript">
+							$().ready(function() {
+								$("#incident_date").datepicker({ 
+									showOn: "both", 
+									buttonImage: "<?php echo url::file_loc('img'); ?>media/img/icon-calendar.gif", 
+									buttonImageOnly: true 
+								});
+							});
+						</script>
+					</div>
+					<div class="time">
+						<h4><?php echo Kohana::lang('ui_main.reports_time'); ?></h4>
+						<?php
+							for ($i=1; $i <= 12 ; $i++)
+							{ 
+								$hour_array[sprintf("%02d", $i)] = sprintf("%02d", $i);	 // Add Leading Zero
+							}
+							for ($j=0; $j <= 59 ; $j++)
+							{ 
+								$minute_array[sprintf("%02d", $j)] = sprintf("%02d", $j);	// Add Leading Zero
+							}
+							$ampm_array = array('pm'=>'pm','am'=>'am');
+							print form::dropdown('incident_hour',$hour_array,$form['incident_hour']);
+							print '<span class="dots">:</span>';
+							print form::dropdown('incident_minute',$minute_array,$form['incident_minute']);
+							print '<span class="dots">:</span>';
+							print form::dropdown('incident_ampm',$ampm_array,$form['incident_ampm']);
+						?>
+						<?php if ($site_timezone != NULL): ?>
+							<small>(<?php echo $site_timezone; ?>)</small>
+						<?php endif; ?>
+					</div>
+					<div style="clear:both; display:block;" id="incident_date_time"></div>
+				</div>
+				<div class="report_row wdif">
+					<h4><?php echo Kohana::lang('ui_main.reports_categories'); ?></h4>
+					<div class="report_category" id="categories">
+					<?php
+						$selected_categories = (!empty($form['incident_category']) AND is_array($form['incident_category']))
+							? $selected_categories = $form['incident_category']
+							: array();
+							
+						$columns = 2;
+						echo category::tree($categories, $selected_categories, 'incident_category', $columns);
+						?>
+					</div>
+				</div>
+
+				<?php
+				// Action::report_form - Runs right after the report categories
+				Event::run('ushahidi_action.report_form');
+				?>
+
+				<?php echo $custom_forms ?>
 				<div class="report_row">
 					<input name="submit" type="submit" value="<?php echo Kohana::lang('ui_main.reports_btn_submit'); ?>" class="btn_submit" /> 
 				</div>
+
+
+				<div class="report_optional wdif">
+					<h3><?php echo Kohana::lang('ui_main.reports_optional'); ?></h3>
+					<div class="report_row">
+            <label for="person_first" class="inlined"><?php echo Kohana::lang('ui_main.reports_first'); ?></label>
+						<?php print form::input('person_first', $form['person_first'], ' class="text long"'); ?>
+					</div>
+					<div class="report_row">
+						<label for="person_last" class="inlined"><?php echo Kohana::lang('ui_main.reports_last'); ?></label>
+						<?php print form::input('person_last', $form['person_last'], ' class="text long"'); ?>
+					</div>
+					<div class="report_row">
+						<label for="person_email" class="inlined"><?php echo Kohana::lang('ui_main.reports_email'); ?></label>
+						<?php print form::input('person_email', $form['person_email'], ' class="text long"'); ?>
+					</div>
+					<?php
+					// Action::report_form_optional - Runs in the optional information of the report form
+					Event::run('ushahidi_action.report_form_optional');
+					?>
+				</div>
 			</div>
-		</div>
+</div>
 		<?php print form::close(); ?>
 		<!-- end report form block -->
 	</div>
